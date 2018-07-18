f1bd808e9894d407e7b6f895a571120ca0fbf09f
CAIO PIMENTEL <caio3191@gmail.com>
2018-07-18 13:17:15 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input = 5)
+def input_to_index(input)
   index = ('input'.to_i) - 1
+  return index
 end
 
 def move(board, index, valeu = "X")
